50a18aece1192d6bfc109887dd6501256205ad5f
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-10-01 14:07:37 -0600

    feat: add background picture

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ const char tile_chars[TILE_TYPES] = {'#', '@', '$', '%', '&'};
 char board[BOARD_SIZE][BOARD_SIZE];
 
 Vector2 grid_origin;
+Texture2D background;
 
 char random_tile() {
     return tile_chars[rand() % TILE_TYPES];
@@ -42,11 +43,33 @@ int main(void) {
     InitWindow(screen_width, screen_height, "Raylib 2D ASCII MATCH 3");
     SetTargetFPS(60);
     srand(time(NULL));
+
+    background = LoadTexture("cosmos.jpeg");
+
     init_board();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        DrawTexturePro(
+            background
+            , (Rectangle) {
+                0
+                , 0
+                , background.width
+                , background.height
+            }
+            , (Rectangle) {
+                0
+                , 0
+                , GetScreenWidth()
+                , GetScreenHeight()
+            }
+            , (Vector2) {0, 0}
+            , 0.0f
+            , WHITE
+        );
 
         for (int y = 0; y < BOARD_SIZE; ++y) {
             for (int x = 0; x < BOARD_SIZE; ++x) {
@@ -72,6 +95,8 @@ int main(void) {
 
         EndDrawing();
     }
+
+    UnloadTexture(background);
 
     CloseWindow();
 
